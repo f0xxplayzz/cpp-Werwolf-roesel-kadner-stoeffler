5fdd5fab30c27e7699ab3c490b66eb8dc0701a5b
f0xxplayzz <f0xxplayzz@gmail.com>
2020-12-29 10:37:22 +0100

    Second

--- Networking/TCP_Connection.cpp
+++ Networking/TCP_Connection.cpp
@@ -1,9 +1,9 @@
-#pragma once 
+//#pragma once 
 #include <boost/asio.hpp>
 #include <vector>
 #include <memory>
 #include <iostream>
-#include <thread>
+//#include <thread>
 
 using tcp = boost::asio::ip::tcp;
 using error_code_t = boost::system::error_code;
@@ -20,6 +20,40 @@ class tcp_connection
     {
         delete[] buf;
         m_sock.close();
+    }
+
+    void test(){
+        std::cout<<"Hallo"<<std::endl;
+    }
+
+    void handlerFunction(){
+        boost::asio::io_service my_io_service;
+        tcp::endpoint endpoint{tcp::v4(), 8999};
+        tcp::acceptor acceptor{my_io_service};
+        acceptor.open(endpoint.protocol());
+        acceptor.bind(endpoint);
+        acceptor.listen();
+        auto con = std::make_shared<tcp_connection>(my_io_service);
+        acceptor.async_accept(con->m_sock, [con](const error_code_t &ec) {
+            if (!ec)
+            {
+
+                auto boost_buf = boost::asio::buffer(con->buf, 100);
+                boost::asio::async_read(con->m_sock, boost_buf, 
+                [con](error_code_t ec, size_t len) {
+                    //std::cout << con->buf;
+                    std::reverse(con->buf, con->buf + 100);
+                    auto buf = boost::asio::buffer(con->buf, 100);
+                    boost::asio::async_write(con->m_sock, buf,
+                        [con](error_code_t ec, size_t len) { 
+                            std::cout << std::endl << "i did my job" << std::endl; });
+                });
+            }
+        });
+        auto thread_func = [&my_io_service]() { my_io_service.run(); };
+        //std::thread t(thread_func);
+        //t.join();
+        acceptor.listen();
     }
 
 };
